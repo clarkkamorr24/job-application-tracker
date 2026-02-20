5cecc0f03cd1b41f1c307fa1c87ef6b4232c05c6
clarkkamorr24 <biancake0803@gmail.com>
2026-02-20 14:18:18 +0800

    chore: setup sign-in and sign-up routes

--- app/page.tsx
+++ app/page.tsx
@@ -1,19 +1,25 @@
+import { Button } from "@/components/ui/button";
+import { ArrowRight } from "lucide-react";
+import Link from "next/link";
+
 export default function Home() {
   return (
-    <div className="flex min-h-screen flex-col bg-white">
+    <div className="flex flex-col min-h-screen bg-white ">
       <main className="flex-1">
-        <section className="container mx-auto px-4 py-32">
-          <div className="mx-auto max-w-4xl text-center">
-            <h1 className="text-black mb-6 text-6xl font-bold">
+        <section className="container px-4 py-32 mx-auto">
+          <div className="max-w-4xl mx-auto text-center">
+            <h1 className="mb-6 text-6xl font-bold text-black ">
               A better way to track your job applications
             </h1>
-            <p className="text-muted-foreground mb-10 text-xl">
+            <p className="mb-10 text-xl text-muted-foreground">
               Capture, organize, and manage your job applications in one place.
             </p>
             <div className="flex flex-col items-center gap-4">
-              <button className="bg-primary text-primary-foreground hover:bg-primary/90 px-4 py-2 rounded-md">
-                Start for free
-              </button>
+              <Link href="/sign-up">
+                <Button size="lg" className="h-12 px-8 text-lg font-medium">
+                  Start for free <ArrowRight className="ml-2" />
+                </Button>
+              </Link>
               <p>Free forever</p>
             </div>
           </div>
