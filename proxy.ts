588749ced1a70e3b66762f924395f003c7828622
clarkkamorr24 <biancake0803@gmail.com>
2026-02-24 13:49:39 +0800

    fix: proxy

--- proxy.ts
+++ proxy.ts
@@ -1,11 +1,13 @@
-// middleware.ts
 import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
 
 export default function proxy(req: NextRequest) {
   const { pathname } = req.nextUrl;
   const token = req.cookies.get("better-auth.session_token")?.value;
-  if (token && (pathname === "/sign-in" || pathname === "/sign-up")) {
+
+  const authPages = ["/sign-in", "/sign-up"];
+
+  if (token && authPages.includes(pathname)) {
     const url = req.nextUrl.clone();
     url.pathname = "/dashboard";
     return NextResponse.redirect(url);
